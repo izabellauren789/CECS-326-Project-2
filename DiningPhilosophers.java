ade1509c7f095933ad099ee148dc2f4e83618318
izabellauren789 <izabellauren789@gmail.com>
2023-10-02 16:00:24 -0700

    adding files

---
+++ DiningPhilosophers.java
@@ -0,0 +1,16 @@
+/**
+ * DiningPhilosophers.java
+ *
+ * This program starts the dining philosophers problem.
+ * Project 2
+ *
+ */
+
+
+public class DiningPhilosophers
+{  
+   public static void main(String args[])
+   {
+     // Here your code
+   }
+}
