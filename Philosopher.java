a36068b4b324ab4f94263071cb7cc9d88d882f5f
euilee0129 <superbankaisam@gmail.com>
2023-10-09 13:54:49 -0700

    Update Philosopher.java

--- Philosopher.java
+++ Philosopher.java
@@ -7,13 +7,6 @@
 public class Philosopher implements Runnable {
    /*
     * pseudocode
-    * need 5 philosophers 0 - 4
-    * Thread t0 = new Thread();
-    * Thread t1 = new Thread();
-    * Thread t2 = new Thread();
-    * Thread t3 = new Thread();
-    * Thread t4 = new Thread();
-    * 
     * each thread sleep for random period between 1 and 3
     * lock for thinking and eating
     */
