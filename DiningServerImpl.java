0650aad7b07f7275e4f276cbf1ee29736e679772
euilee0129 <superbankaisam@gmail.com>
2023-10-13 15:33:45 -0700

    Update DiningServerImpl.java

--- DiningServerImpl.java
+++ DiningServerImpl.java
@@ -14,7 +14,7 @@ public class DiningServerImpl implements DiningServer {
 	public void takeForks(int num){
 		for(int i =0; i<5; i++){
 			forks[i] = new Semaphore(1);
-			forks[(i+1)%4] = new Semaphore(1);
+			forks[(i+1)%5] = new Semaphore(1); //4+1 = 5, 5 not index 5th has to be 0
 		}
 	}
 	public void returnForks(int num){
